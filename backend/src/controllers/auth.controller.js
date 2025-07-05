7091af0c8be3ff059dbb467321f3ff283be40855
Medatarunvenkat <tarunvm0909@gmail.com>
2025-07-05 21:47:37 +0530

    backend

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -104,3 +104,12 @@ export const login = async (req, res) => {
       res.status(500).json({ message: "Internal server error" });
     }
   };
+
+  export const checkAuth = (req, res) => {
+    try {
+      res.status(200).json(req.user);
+    } catch (error) {
+      console.log("Error in checkAuth controller", error.message);
+      res.status(500).json({ message: "Internal Server Error" });
+    }
+  };
