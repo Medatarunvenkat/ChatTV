b43fb4ae773cf7c70a9300bcf23ac8693006c0a6
Medatarunvenkat <tarunvm0909@gmail.com>
2025-07-05 20:36:32 +0530

    profile added

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,9 +1,11 @@
 import express from "express";
+import { login, logout, signup, updateProfile } from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 const router=express.Router();
-import { signup, login, logout } from "../controllers/auth.controller.js";
 
 router.post("/signup",signup);
 router.post("/login",login);
 router.post("/logout",logout);
-router.put("/update-profile",updateProfile);
+router.put("/update-profile",protectRoute,updateProfile);
+router.get
 export default router;
